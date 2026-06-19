af6132ab66e9d269aca7e2c02093ec4635574e4b
Jose Martinez <jmartinez@fireflyspace.com>
2026-06-19 11:39:49 -0500

    Update unit tests

--- Svc/Ccsds/CfdpManager/test/ut/CfdpManagerTester.cpp
+++ Svc/Ccsds/CfdpManager/test/ut/CfdpManagerTester.cpp
@@ -957,7 +957,7 @@ void CfdpManagerTester::sendAndVerifyClass2Tx(
         this->component.doDispatch();
 
         // Run cycles until second EOF
-        U32 maxCycles = 10;
+        U32 maxCycles = 20;
         bool foundSecondEof = false;
 
         for (U32 cycle = 0; cycle < maxCycles && !foundSecondEof; ++cycle) {
